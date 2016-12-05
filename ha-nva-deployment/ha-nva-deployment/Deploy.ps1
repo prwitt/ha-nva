f5d485161ccdc7b98602774693d1f7a5bbc1e9e0
Telmo Sampaio <telmos@microsoft.com>
2016-12-05 10:12:53 -0800

    Removed "test" from names and changed dmz to two VMs

--- ha-nva-deployment/ha-nva-deployment/Deploy.ps1
+++ ha-nva-deployment/ha-nva-deployment/Deploy.ps1
@@ -32,42 +32,47 @@ $templateRootUri = New-Object System.Uri -ArgumentList @($buildingBlocksRootUriS
 $virtualNetworkTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/vnet-n-subnet/azuredeploy.json")
 $loadBalancerTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/loadBalancer-backend-n-vm/azuredeploy.json")
 $multiVMsTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/multi-vm-n-nic-m-storage/azuredeploy.json")
-$dmzTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/dmz/azuredeploy.json")
 $udrTemplate = New-Object System.Uri -ArgumentList @($templateRootUri, "templates/buildingBlocks/userDefinedRoutes/azuredeploy.json")
 
-$virtualNetworkParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-test-vnet.parameters.json")
-$webSubnetLoadBalancerAndVMsParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-test-web.parameters.json")
-$mgmtSubnetVMsParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-test-mgmt.parameters.json")
-$dmzParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-test-dmz.parameters.json")
-$zooParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-test-zoo.parameters.json")
-$udrParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-test-udr.parameters.json")
+$virtualNetworkParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-vnet.parameters.json")
+$webSubnetLoadBalancerAndVMsParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-web.parameters.json")
+$mgmtSubnetVMsParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-mgmt.parameters.json")
+$nva1ParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva1.parameters.json")
+$nva2ParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva2.parameters.json")
+$zooParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-zoo.parameters.json")
+$udrParametersFile = [System.IO.Path]::Combine($PSScriptRoot, "parameters\ha-nva-udr.parameters.json")
 
 # Login to Azure and select your subscription
 Login-AzureRmAccount -SubscriptionId $SubscriptionId | Out-Null
 
 if($Mode -eq "All" -Or $Mode -eq "Infrastructure"){
 	# Create the resource group
-	#$networkResourceGroup = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+	$networkResourceGroup = New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
 
 	Write-Host "Deploying virtual network..."
 	#New-AzureRmResourceGroupDeployment -Name "ra-vnet-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
-		-TemplateUri $virtualNetworkTemplate.AbsoluteUri -TemplateParameterFile $virtualNetworkParametersFile
+	#	-TemplateUri $virtualNetworkTemplate.AbsoluteUri -TemplateParameterFile $virtualNetworkParametersFile
 
 	Write-Host "Deploying load balancer and virtual machines in web subnet..."
 	#New-AzureRmResourceGroupDeployment -Name "ra-web-lb-vms-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
-		-TemplateUri $loadBalancerTemplate.AbsoluteUri -TemplateParameterFile $webSubnetLoadBalancerAndVMsParametersFile
+	#	-TemplateUri $loadBalancerTemplate.AbsoluteUri -TemplateParameterFile $webSubnetLoadBalancerAndVMsParametersFile
 
 	Write-Host "Deploying jumpbox in mgmt subnet..."
 	#New-AzureRmResourceGroupDeployment -Name "ra-mgmt-vms-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
-		-TemplateUri $multiVMsTemplate.AbsoluteUri -TemplateParameterFile $mgmtSubnetVMsParametersFile
+	#	-TemplateUri $multiVMsTemplate.AbsoluteUri -TemplateParameterFile $mgmtSubnetVMsParametersFile
 	
-	Write-Host "Deploying dmz..."
-	New-AzureRmResourceGroupDeployment -Name "ra-dmz-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
-		-TemplateUri $dmzTemplate.AbsoluteUri -TemplateParameterFile $dmzParametersFile
+	Write-Host "Deploying nva1..."
+	New-AzureRmResourceGroupDeployment -Name "ra-nva1-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
+		-TemplateUri $multiVMsTemplate.AbsoluteUri -TemplateParameterFile $nva1ParametersFile
+
+	Write-Host "Deploying nva2..."
+	New-AzureRmResourceGroupDeployment -Name "ra-nva1-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
+		-TemplateUri $multiVMsTemplate.AbsoluteUri -TemplateParameterFile $nva2ParametersFile
+
 
 	Write-Host "Deploying udr..."
 	#New-AzureRmResourceGroupDeployment -Name "ra-udr-deployment" -ResourceGroupName $networkResourceGroup.ResourceGroupName `
-		-TemplateUri $udrTemplate.AbsoluteUri -TemplateParameterFile $udrParametersFile
+	#	-TemplateUri $udrTemplate.AbsoluteUri -TemplateParameterFile $udrParametersFile
 }
 
 if($Mode -eq "All" -Or $Mode -eq "Docker"){
